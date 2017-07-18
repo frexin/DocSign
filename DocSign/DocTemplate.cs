eb3e717b578eb296643bf4974b462139d5355eee
Kirill <sks89@mail.ru>
2017-07-18 18:55:31 +0300

    init

---
+++ DocSign/DocTemplate.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Novacode;
+using System.IO;
+
+namespace DocSign
+{
+    class DocTemplate
+    {
+        protected Dictionary<string, string> postData;
+        protected string[] monthsNames;
+
+        public DocTemplate(string rawData)
+        {
+            postData = DecodePostData(rawData);
+            monthsNames = new string[] { "января", "февраля", "марта", "апреля", "мая", "июня", "июля",
+            "августа", "сентября", "октября", "ноября", "декабря"};
+        }
+
+        public Dictionary<string, string> GetPostData()
+        {
+            return postData;
+        }
+
+        public Dictionary<string, string> GetDocDict()
+        {
+            return DecodeDocSubstitutions();
+        }
+
+        public string ProcessFile(string templatePath)
+        {
+            Dictionary<string, string> replaces = GetDocDict();
+
+            string monthString = replaces["%MON%"];
+            int monthIndex = Int32.Parse(monthString) - 1;
+            string monthName = monthsNames[monthIndex];
+
+            replaces["%MON%"] = monthName;
+
+            var doc = DocX.Load(templatePath);
+
+            foreach (KeyValuePair<string, string> entry in replaces)
+            {
+                doc.ReplaceText(entry.Key, entry.Value);
+            }
+
+            string tempFileName = GenerateTempFilePath();
+            doc.SaveAs(tempFileName);
+
+            return tempFileName;
+        }
+
+        private Dictionary<string, string> DecodePostData(string postData)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            char delimiter = '&';
+            string[] parts = postData.Split(delimiter);
+
+            foreach (string part in parts)
+            {
+                delimiter = '=';
+                string[] keyval = part.Split(delimiter);
+
+                result.Add(keyval[0], keyval[1]);
+            }
+
+            return result;
+        }
+
+        private Dictionary<string, string> DecodeDocSubstitutions()
+        {
+            Dictionary <string, string> result = new Dictionary<string, string>();
+
+            if (postData.ContainsKey("template"))
+            {
+                string rawJson = postData["template"];
+                result = JsonConvert.DeserializeObject<Dictionary<string, string>>(rawJson);
+            }
+
+            return result;
+        }
+
+        private string GenerateTempFilePath()
+        {
+            string randomString = Guid.NewGuid().ToString().Substring(0, 8);
+            string tempPath = Path.GetTempPath();
+
+            string result = tempPath + "\" + randomString + ".docx";
+
+            return result;
+        }
+    }
+}
---
+++ DocSign/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Novacode;
+using FastCGI;
+
+namespace DocSign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Content-type: text/html\n\n"); // support CGI mode
+
+            string rawData = Console.ReadLine();
+            DocTemplate docTemplate = new DocTemplate(rawData);
+            Dictionary <string, string> docDict = docTemplate.GetDocDict();
+            Dictionary<string, string> postData = docTemplate.GetPostData();
+
+            foreach (KeyValuePair<string, string> entry in postData)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", entry.Key, entry.Value);
+            }
+
+            foreach (KeyValuePair<string, string> docPart in docDict)
+            {
+                Console.WriteLine("DocKey = {0}, DocValue = {1}", docPart.Key, docPart.Value);
+            }
+
+            string filePath = docTemplate.ProcessFile(postData["path"]);
+            Console.WriteLine("File Path: " + filePath);
+        }
+
+        private static void App_OnRequestReceived(object sender, Request request)
+        {
+            var responseString =
+                  "HTTP/1.1 200 OK\n"
+                + "Content-Type:text/html\n"
+                + "\n"
+                + "Hello World!";
+
+            request.WriteResponseASCII(responseString);
+            request.Close();
+        }
+
+        public void TemplateDoc(string[] args)
+        {
+            string templateFileName = @"D:\docs\sogl_template.docx";
+            string outputFileName = @"D:\docs\out.docx";
+            string clientName = args[0];
+
+            var doc = DocX.Load(templateFileName);
+            doc.ReplaceText("%CLIENT%", clientName);
+            doc.SaveAs(outputFileName);
+        }
+    }
+}
